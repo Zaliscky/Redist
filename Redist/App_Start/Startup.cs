93edfcc93d63449491bd26f5d2ca296b120f4a04
Zaliscky <zalisckyivan@gmail.com>
2018-12-09 18:06:07 +0200

    Init commit

---
+++ Redist/App_Start/Startup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin;
+using Owin;
+using Redist.Models;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.AspNet.Identity;
+
+[assembly: OwinStartup(typeof(Redist.App_Start.Startup))]
+
+namespace Redist.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // настраиваем контекст и менеджер
+            app.CreatePerOwinContext<ApplicationContext>(ApplicationContext.Create);
+            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+            });
+        }
+    }
+}
---
+++ Redist/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Redist.MvcApplication" Language="C#" %>
